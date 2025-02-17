a0df9ac2074ae53b5fb235f4303db4eb249c5947
vahghar <raghav10g@gmail.com>
2025-02-18 01:15:16 +0530

    removed-welcome

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -13,12 +13,6 @@ export default function AuthLayout({
 }) {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Welcome
-        </h2>
-      </div>
-
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="py-8 px-4  sm:px-10">
           {children}
